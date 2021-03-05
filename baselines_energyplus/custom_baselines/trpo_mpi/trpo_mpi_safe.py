3be6db1c9720f0796f25cdd9c26b2ec1ee1bdb81
Johannes Rehm <johannes.rehm@ntnu.no>
2021-03-05 14:33:58 +0100

    added a basic PID controller

--- baselines_energyplus/custom_baselines/trpo_mpi/trpo_mpi_safe.py
+++ baselines_energyplus/custom_baselines/trpo_mpi/trpo_mpi_safe.py
@@ -10,6 +10,106 @@ from baselines.common.mpi_adam import MpiAdam
 from baselines.common.cg import cg
 from contextlib import contextmanager
 
+import time
+
+class PID:
+    """PID Controller
+    """
+
+    def __init__(self, P=0.11, I=0.01, D=0.2, current_time=None):
+
+        self.Kp = P
+        self.Ki = I
+        self.Kd = D
+
+        self.sample_time = 0.00
+        self.current_time = current_time if current_time is not None else time.time()
+        self.last_time = self.current_time
+
+        self.clear()
+
+    def clear(self):
+        """Clears PID computations and coefficients"""
+        self.SetPoint = 0.0
+
+        self.PTerm = 0.0
+        self.ITerm = 0.0
+        self.DTerm = 0.0
+        self.last_error = 0.0
+
+        # Windup Guard
+        self.int_error = 0.0
+        self.windup_guard = 20.0
+
+        self.output = 0.0
+
+    def update(self, feedback_value, current_time=None):
+        """Calculates PID value for given reference feedback
+
+        .. math::
+            u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
+
+        .. figure:: images/pid_1.png
+           :align:   center
+
+           Test PID with Kp=1.2, Ki=1, Kd=0.001 (test_pid.py)
+
+        """
+        error = self.SetPoint - feedback_value
+
+        self.current_time = current_time if current_time is not None else time.time()
+        delta_time = 1#self.current_time - self.last_time
+        delta_error = error - self.last_error
+
+        if (delta_time >= self.sample_time):
+            self.PTerm = self.Kp * error
+            self.ITerm += error * delta_time
+
+            if (self.ITerm < -self.windup_guard):
+                self.ITerm = -self.windup_guard
+            elif (self.ITerm > self.windup_guard):
+                self.ITerm = self.windup_guard
+
+            self.DTerm = 0.0
+            if delta_time > 0:
+                self.DTerm = delta_error / delta_time
+
+            # Remember last time and last error for next calculation
+            self.last_time = self.current_time
+            self.last_error = error
+
+            self.output = self.PTerm + (self.Ki * self.ITerm) + (self.Kd * self.DTerm)
+
+    def setKp(self, proportional_gain):
+        """Determines how aggressively the PID reacts to the current error with setting Proportional Gain"""
+        self.Kp = proportional_gain
+
+    def setKi(self, integral_gain):
+        """Determines how aggressively the PID reacts to the current error with setting Integral Gain"""
+        self.Ki = integral_gain
+
+    def setKd(self, derivative_gain):
+        """Determines how aggressively the PID reacts to the current error with setting Derivative Gain"""
+        self.Kd = derivative_gain
+
+    def setWindup(self, windup):
+        """Integral windup, also known as integrator windup or reset windup,
+        refers to the situation in a PID feedback controller where
+        a large change in setpoint occurs (say a positive change)
+        and the integral terms accumulates a significant error
+        during the rise (windup), thus overshooting and continuing
+        to increase as this accumulated error is unwound
+        (offset by errors in the other direction).
+        The specific problem is the excess overshooting.
+        """
+        self.windup_guard = windup
+
+    def setSampleTime(self, sample_time):
+        """PID that should be updated at a regular interval.
+        Based on a pre-determined sampe time, the PID decides if it should compute or return immediately.
+        """
+        self.sample_time = sample_time
+
 
 def manual_controller(ac, ob, flow_baseline=0.0):
     if ob[1] > 23.6:
@@ -69,6 +169,11 @@ def traj_segment_generator(pi, env, horizon, stochastic, use_manual_controller_i
     ep_rets = []
     ep_lens = []
 
+    pid_w = PID()
+    pid_w.SetPoint = 19.5
+    pid_e = PID()
+    pid_e.SetPoint = 19.5
+
     # Initialize history arrays
     obs = np.array([ob for _ in range(horizon)])
     rews = np.zeros(horizon, 'float32')
@@ -98,9 +203,14 @@ def traj_segment_generator(pi, env, horizon, stochastic, use_manual_controller_i
         # prevcontact = controler_ac
         # if use the manual controller:
         if use_manual_controller_integration:
-            controler_ac = manual_controller_v2(ac=controler_ac, ob=ob)
+            #controler_ac = manual_controller(ac=controler_ac, ob=ob, flow_baseline=1.0)
+            pid_w.update(ob[1])
+            pid_e.update(ob[2])
+            controler_ac = np.clip(np.array([pid_w.output, pid_e.output, 0.7, 0.7]), a_min =[-1.0, -1.0, -0.5, -0.5], a_max = [1.0, 1.0, 1.0, 1.0])
+
             # add the manual controller action to ob
             ob[6:-1] = controler_ac
+
             policy_ac, vpred = pi.act(stochastic, ob)
             prevac = policy_ac
             # scale the policy_action using the manual_mode_only
